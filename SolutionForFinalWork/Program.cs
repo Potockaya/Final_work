168231761404e3d4bb95acf624bba4447e85a0f8
«Marina <marina1904@list.ru>
2022-09-10 00:03:13 +0300

    add second solution for final work

--- SolutionForFinalWork/Program.cs
+++ SolutionForFinalWork/Program.cs
@@ -1,14 +1,53 @@
-﻿Console.Clear();
-Console.WriteLine("Введите слова, символы или цифры через запятую");
-string inputLine = Console.ReadLine();
+﻿// Console.Clear();
+// Console.WriteLine("Введите слова, символы или цифры через запятую");
+// string inputLine = Console.ReadLine();
 
-string[] words = inputLine.Split(new char[] { ',' });
-foreach (string s in words)
-{
-    if (s.Length<=3)
-    {
-        Console.WriteLine(s);
-    }
+// string[] words = inputLine.Split(new char[] { ',' });
+// foreach (string s in words)
+// {
+//     if (s.Length<=3)
+//     {
+//         Console.WriteLine(s);
+//     }
     
-}
+// }
 
+string[] inputLine =  {"hello", "two", "world", ")))","Russia", "Denmark", "Kazan","1234", "1567", "-2", "computer science"};
+int countInputLine = CountLessThree(inputLine);
+string[] result =  FillResultArray(inputLine,countInputLine);
+
+void PrintArray(string[] array) 
+    {
+        Console.Write("[");
+        for (int i = 0; i < array.Length; i++)
+            {
+                if (i != array.Length - 1) Console.Write("\""+array[i]+"\""+",");
+            }
+        Console.Write("\""+$"{array[array.Length - 1]}"+"\""+"]");
+    }
+
+int CountLessThree(string[] array)
+    {
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= 3) count++;
+            }
+        return count;
+    }
+
+string[] FillResultArray(string[] array, int count)
+    {
+        string[] result = new string[count];
+        for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= 3)
+                    {
+                        result[result.Length - count] = array[i];
+                        count--;
+                    }
+            }
+        return result;
+    }
+
+PrintArray(result);
